a9ccf6178142a5bca6c4b521d68e1ebbeb2bf1c3
Jacob-Newman <98132190+Jacob-Newman@users.noreply.github.com>
2024-11-18 12:52:42 -0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -2,9 +2,22 @@
 export default function Home() {
   return (
     <div>
-      
+      <div className = "HomePage">
+
+        <div className = "browseRestaurants">
+          <button className = "browseRestaurants-button">Browse Restaurants</button>
+        </div>
 
 
+        <div className = "signIn">
+          <button className = "signIn-button">Sign In</button>
+        </div>
+
+        <div className = "HomePage-Title">
+          <p>Tables4U</p>
+        </div>
+
+      </div>
     </div>
   );
 }
