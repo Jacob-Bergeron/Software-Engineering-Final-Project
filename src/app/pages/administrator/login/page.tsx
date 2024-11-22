486e24a0c9da77a879d43fe61e65dec71534b666
Michael Monda <michaeljmonda@outlook.com>
2024-11-22 12:01:53 -0500

    reorganizing pages

---
+++ src/app/pages/administrator/login/page.tsx
@@ -0,0 +1,32 @@
+import './style.css';
+import Link from 'next/link';
+
+
+export default function loginpage() {
+    return (
+        <div>
+        <div className = "login-page">
+            <label className = "Username">Username </label>
+            <input type="text" className = "UsernameInput" placeholder="Enter Username"/>
+            <label className = "Password">Password </label>
+            <input type="text" className = "PasswordInput" placeholder="Enter Password"/>
+            <button className = "login-button">Sign In</button>
+            <label className = "register">Don't have an account?</label>
+            <button className = "register-button">Sign up here</button>
+        </div>
+        <div>
+        
+        </div>
+
+        <div className = "back-button-login">
+            <p>
+                <Link href="/" className = "back-button">Back</Link>
+            </p>
+        </div>
+        
+        </div>
+
+    );
+
+
+}
