16c2946e4e60fe9ea793ffbb8823b9c647892844
SkylerDooley <skylerdooley@gmail.com>
2024-11-25 15:15:42 -0500

    updated all lambda functions index pages

--- lambdaFunctions/activateRestaurant/index.mjs
+++ lambdaFunctions/activateRestaurant/index.mjs
@@ -1,5 +1,6 @@
 import mysql from 'mysql'
 
+
 export const handler = async (event) => {
     var pool = mysql.createPool({
         host: "cs3733db.c5ia86k2epli.us-east-2.rds.amazonaws.com",
@@ -9,19 +10,38 @@ export const handler = async (event) => {
     });
 
 
-    let activeRestaurant = () => {
-        
-    }
+    let activateRestaurant = (res_UUID) => {
+      return new Promise((resolve, reject) => {
+          pool.query(
+            "UPDATE All_Restaurants SET isActive = 1 WHERE res_UUID = ?",
+            [res_UUID],
+            (error, rows) => {
+              if (error) {
+                return reject(error);
+              }
+              return resolve(rows);
+            }
+          );
+        });
+  }
 
+
+  let response; 
     // this is what is returned to client
-    const response = {
-        statusCode: 200,
-        result: {
-            
-        }
+    try{
+        const activated = await activateRestaurant(event.res_UUID)
+        response = {
+            statusCode: 200,
+            "message" : "successfully activated"
+        };
+    } catch(error){
+        response ={
+            statusCode : 400,
+            message : "Internal Error",
+            error : error.message
+        };
     }
 
     pool.end()      // close DB connections
-
     return response;
 }
