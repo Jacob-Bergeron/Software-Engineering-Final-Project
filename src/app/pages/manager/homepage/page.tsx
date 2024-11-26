811056cebf82132906851c302353ed0467ffd0ea
Jacob Bergeron <jacobbergeron07@gmail.com>
2024-11-26 15:39:48 -0500

    to local

--- src/app/pages/manager/homepage/page.tsx
+++ src/app/pages/manager/homepage/page.tsx
@@ -16,7 +16,7 @@ export default function managerHomePage() {
 
   const andRefreshDisplay = () => {
     forceRedraw(redraw + 1)
-}
+  }
 
   const [isActive, setisActive] = React.useState(false);
   const [restaurantName, setrestaurantName] = React.useState(null)
@@ -26,7 +26,7 @@ export default function managerHomePage() {
   const [res_UUID, setres_UUID] = React.useState(null)
 
 
-  
+
   const queryParams = new URLSearchParams(window.location.search);
   const managerString = queryParams.get('manager');
   const manager = managerString ? JSON.parse(decodeURIComponent(managerString)) : null;
@@ -43,32 +43,32 @@ export default function managerHomePage() {
     "username": username
   }).then(function (response) {
 
-        setres_UUID(response.data.result.body[0].res_UUID)
+    setres_UUID(response.data.result.body[0].res_UUID)
 
-          // get restaurant data from the All_Restaurants table
-        instance.post('/managerGetRestaurantData', {
-          "res_UUID": res_UUID
-        }).then(function (response) {
-          let status = response.data.statusCode
+    // get restaurant data from the All_Restaurants table
+    instance.post('/managerGetRestaurantData', {
+      "res_UUID": res_UUID
+    }).then(function (response) {
+      let status = response.data.statusCode
 
 
-          if (status == 200) {
-            activity = response.data.result.body[0].isActive
+      if (status == 200) {
+        activity = response.data.result.body[0].isActive
 
-            setrestaurantName(response.data.result.body[0].restaurantName || "No Name")
-            setAddress(response.data.result.body[0].address || "No Address")
-            setstartTime(response.data.result.body[0].openTime || "null")
-            setcloseTime(response.data.result.body[0].closeTime || "null")
+        setrestaurantName(response.data.result.body[0].restaurantName || "No Name")
+        setAddress(response.data.result.body[0].address || "No Address")
+        setstartTime(response.data.result.body[0].openTime || "null")
+        setcloseTime(response.data.result.body[0].closeTime || "null")
 
-            // change the display if the restaurant is active
-            if (activity == 1) {
-              setisActive(true)
-            }
-          }
+        // change the display if the restaurant is active
+        if (activity == 1) {
+          setisActive(true)
+        }
+      }
 
-        }).catch(function (error) {
-          console.log(error)
-        })
+    }).catch(function (error) {
+      console.log(error)
+    })
 
   }).catch(function (error) {
     console.log(error)
@@ -81,47 +81,63 @@ export default function managerHomePage() {
     let openTimeInput = document.getElementById("openingTime") as HTMLInputElement
     let closeTimeInput = document.getElementById("closingingTime") as HTMLInputElement
 
-        instance.post('/restaurant/editTime', {
-            "openingTime": openTimeInput.value, "closingTime": closeTimeInput.value
-        })
-        .then(function (response) {
-
-          
-          andRefreshDisplay()
-        })
-        .catch(function (error) {
-            console.log(error)
-        })   
-}
+    instance.post('/restaurant/editTime', {
+      "openingTime": openTimeInput.value, "closingTime": closeTimeInput.value
+    })
+      .then(function (response) {
 
 
-function ActiveRestaurant() {
-  instance.post('/activateRestaurant', {
+        andRefreshDisplay()
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+
+
+  function ActiveRestaurant() {
+    instance.post('/activateRestaurant', {
       "res_UUID": res_UUID
-  })
+    })
       .then(function (response) {
 
       })
       .catch(function (error) {
-          console.log(error)
+        console.log(error)
       })
-}
+  }
 
-function retrieveTables(){
-  instance.post('/', {
-    "res_UUID" : res_UUID
-  }).then(function (response) {
+  function RetrieveTables() {
+    instance.post('/', {
+      "res_UUID": res_UUID
+    }).then(function (response) {
+      let status = response.data.statusCode
 
-
-  }).catch(function(error) {
-    console.log(error)
-  })
-}
-
+      if (status == 200) {
+        
+      }
 
 
+    }).catch(function (error) {
+      console.log(error)
+    })
+  }
 
-// HTML
+  function TablesList(props:any) {
+    if(!props.tables) return <div>No tables to display</div>
+    return (
+    <ul>
+      {
+      // list of tables
+      }
+    </ul>
+    )
+  }
+
+
+
+
+  // HTML
   if (isActive) {
     return (
       <div>
@@ -147,8 +163,10 @@ function retrieveTables(){
     return (
       <div>
         <div className="inactive-restaurant">
+          {/* Signs */}
           <label className="inactive-sign">INACTIVE</label>
           <label className="changeTime-sign">Change Schedule</label>
+          {/* Signs */}
           <div className="restaurant-info">
             <label className="restaurant-name">Restaurant Name: {restaurantName}</label>
             <label className="restaurant-address">Address: {address}</label>
@@ -156,21 +174,21 @@ function retrieveTables(){
             <label className="restaurant-closetime">Closes at: {closeTime}</label>
           </div>
           <div className="time-edits">
-                    <label className = "openTime-label">Opening Time </label>
-                    <input className = "openTime-input" id="openingTime" type="text" placeholder="Enter Opening Time" />
-                    <label className = "closeTime-label">Closing Time</label>
-                    <input className = "closeTime-input"id="closingTime" type="text" placeholder="Enter Closing Time" />
-                    <button className = "submit-changes" onClick={(e) => EditRestaurantTime}>Submit Changes</button>
-            </div>
+            <label className="openTime-label">Opening Time </label>
+            <input className="openTime-input" id="openingTime" type="text" placeholder="Enter Opening Time" />
+            <label className="closeTime-label">Closing Time</label>
+            <input className="closeTime-input" id="closingTime" type="text" placeholder="Enter Closing Time" />
+            <button className="submit-changes" onClick={(e) => EditRestaurantTime}>Submit Changes</button>
+          </div>
+          <div className="activate">
+            <button className="active-button" onClick={(e) => ActiveRestaurant()}>Activate</button>
+          </div>
         </div>
+
+        <TablesList ></TablesList>
+      </div>
             
-        
 
-              <div className="activate">
-              <button onClick={(e) => ActiveRestaurant()}>Activate</button>
-              </div>
-
-      </div >
     );
   }
 }
