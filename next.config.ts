da22288cbf9f75c649aced1ec41ac412fd77e8c0
Jacob Bergeron <jacobbergeron07@gmail.com>
2024-12-02 12:16:39 -0500

    no clue

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output : 'export',
-  basePath : '/psiren-tables4u'
+  basePath : 'psiren-tables4u'
 };
 
 export default nextConfig;
