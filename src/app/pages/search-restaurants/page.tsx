c264db5dbd41698e1d0e554f88c518e7d4f4748e
Jacob Bergeron <jacobbergeron07@gmail.com>
2024-12-03 17:54:01 -0500

    consumer search-restaurants prototype done

--- src/app/pages/search-restaurants/page.tsx
+++ src/app/pages/search-restaurants/page.tsx
@@ -1,3 +1,20 @@
+//? 12/3 
+//? Lambda function and API working with the following payload
+//? Does display on GUI, given this payload as well
+/*
+    {
+    "date" : "2024-12-25",
+    "time" : "1700",
+    "numPeople" : 4
+    }
+ */
+
+//! 12/3 
+//! Have not tested:
+    //! Did not try to request more people than 
+//! Note: Pay attention to how data is structured in tables
+
+
 'use client'                     // NEED THIS to be able to embed HTML in TSX file
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
@@ -16,35 +33,67 @@ What it does:
 */
 export default function SearchRestaurants() {
 
+    // Refresh 
+    const [redraw, forceRedraw] = React.useState(0)
+
+    const andRefreshDisplay = () => {
+        forceRedraw(redraw + 1)
+    }
+
     // State Variables
-    const [futureDateInput, setDateInput] = React.useState("")
+    const [dateInput, setDateInput] = React.useState("")
     const [timeInput, setTimeInput] = React.useState("")
     const [peopleInput, setPeopleInput] = React.useState('')
 
+    // State to hold restaurant data
+    const [restaurants, setRestaurants] = useState<any[]>([]);
+
     // get the current date when the code is called
-    const [date, setDate] = React.useState(new Date().getDate())
-  
+    const [currentDate, setCurrentDate] = React.useState(new Date())
 
-    //! TODO: Implement API and Lambda 
-    // AWS API Call
-    instance.post('/consumerSearchRestaurants', {
-        "currentDate": date, "futureDate": futureDateInput, "time": timeInput, "numPeople": peopleInput
-    }).then(function (response) {
-        let status = response.data.statusCode
+    /*
+    function: searchAvailable()
+    */
+    function searchAvailable() {
         
-        //if successful in reaching database
-        if(status == 200){
+        // AWS API Call
+        instance.post('/consumerSearchRestaurants', {
+            "date": dateInput, "time": timeInput, "numPeople": peopleInput
+        }).then(function (response) {
+            let status = response.data.statusCode
 
-        }
-        // if NOT successful in reahing database
-        else{
-            alert("Could not query")
-        }
+            //if successful in reaching database AND there is something in the payload coming to client
+            if (status == 200 && response.data.result) {
 
-    }).catch(function (error) {
-        console.log(error)
-    })
+                //loop through the tables that are returned
+                for (let i = 0; i < response.data.result.body.length; i++) {
+                    // if the current date is after the dateInput
+                    //! 12/3 Make sure that date types can be compared
+                    if ((response.data.result.body[i].date) >= currentDate) {
+                        alert("Can only search for future days!")
+                    }
+                    // else set the Restaurants into state
+                    else {
+                        //! 12/3 Verify that this works.
+                        //! May need to use JSON.parse()
+                        setRestaurants(response.data.result.body)
+                    }
+                }
+            }
+            // if NOT successful in reaching database
+            else {
+                alert("Could not query")
+            }
 
+        }).catch(function (error) {
+            console.log(error)
+        })
+    }
+
+    // Refreshes display anytime there is a change in [restaurants]
+    useEffect(() => {
+        andRefreshDisplay();
+    }, [restaurants]);
 
     // Return
     return (
@@ -56,23 +105,33 @@ export default function SearchRestaurants() {
 
             <div>
                 <label>Date</label>
-                <input id="dateID" onChange={(e) => setDateInput(e.target.value)} placeholder="Input date" />
+                <input onChange={(e) => setDateInput(e.target.value)} placeholder="Input date" style={{ color: "black" }} />
             </div>
 
             <div>
                 <label>Time</label>
-                <input id="timeID" onChange={(e) => setTimeInput(e.target.value)} placeholder="Input time" />
+                <input onChange={(e) => setTimeInput(e.target.value)} placeholder="Input time" style={{ color: "black" }} />
             </div>
 
             <div>
                 <label>Number of People</label>
-                <input id="peopleID" onChange={(e) => setPeopleInput(e.target.value)} placeholder="Input number of people" />
+                <input onChange={(e) => setPeopleInput(e.target.value)} placeholder="Input number of people" style={{ color: "black" }} />
             </div>
 
-            <button>Submit</button>
+            <button onClick={(e) => searchAvailable()}>Submit</button>
+
 
             <div>
                 <h1>Available Restaurants</h1>
+                <ul>
+                    {restaurants.map((restaurant) => (
+                        <li key={restaurant.res_UUID}>
+                            <h3>Restaurant Name: {restaurant.restaurantName}</h3>
+                            <p>Table ID: {restaurant.table_UUID}</p>
+                            <p>Date: {(restaurant.date)}</p>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
