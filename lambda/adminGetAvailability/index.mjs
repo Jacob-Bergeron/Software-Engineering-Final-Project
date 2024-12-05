245176581b5813c1cd9bca2815f1d7af40bd83c6
Michael Monda <michaeljmonda@outlook.com>
2024-12-05 13:52:48 -0500

    updated admin get avail lambda function

--- lambda/adminGetAvailability/index.mjs
+++ lambda/adminGetAvailability/index.mjs
@@ -1,25 +1,38 @@
-let deleteRestaurant = (res_id, username, password) => {
-    return new Promise((resolve, reject) => {
-        if (username == "admin" && password == "password") {
-            pool.query("SELECT table_UUID FROM TableInfo WHERE res_UUID = ?", 
-            [ res_id, username, password], (error, rows) => {
-                if (error) { 
-                    return reject(error); 
-                }
-                
-                // If the first query is successful, execute the second query
-                pool.query("SELECT * FROM AvailabilityInfo WHERE table_UUID = ?", 
-                [ res_id ], (error, result) => {
-                    if (error) { 
-                        return reject(error); 
-                    }
-                    
-                    // Resolve with the result of the second query
-                    return resolve(result);
-                });
-            });
-        } else {
-            return reject(new Error("unauthorized user"));
-        }
+import mysql from 'mysql'
+
+export const handler = async (event) => {
+    var pool = mysql.createPool({
+        host: "cs3733db.c5ia86k2epli.us-east-2.rds.amazonaws.com",
+        user: "cs3733",
+        password: "database720$",
+        database: "Tables4u"
     });
-}
+
+    //replace this with the new function for the adminGetAvailability lambda function.
+    let adminGetAvailability = (res_id) => {
+        return new Promise((resolve,reject) => {
+            pool.query("SELECT * FROM TableInfo WHERE res_UUID = ?", 
+            [res_id], (error, rows) => {
+                if (error) { return reject(error); }
+                return resolve(rows);
+            })
+        })
+    }
+    try { 
+        const availabilityData = await adminGetAvailability(event.res_UUID); 
+        const response = { 
+            statusCode: 200,
+            result: 
+                " : event.table_UUID"
+            } 
+        return response; 
+    } catch (error) { 
+        const response = { 
+            statusCode: 400, 
+            message: "Internal Error", 
+            error: error.message }; 
+        return response; 
+    } finally { 
+        pool.end(); 
+    } 
+};
