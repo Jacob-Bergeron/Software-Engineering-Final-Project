a0fbc6c7cfa959cada8461f090b21a683ba85e7e
Jacob-Newman <98132190+Jacob-Newman@users.noreply.github.com>
2024-12-06 13:44:59 -0500

    active works

--- src/app/pages/manager/active-homepage/page.tsx
+++ src/app/pages/manager/active-homepage/page.tsx
@@ -26,6 +26,8 @@ export default function managerHomePage() {
     const [startTime, setstartTime] = React.useState("")
     const [closeTime, setcloseTime] = React.useState("")
     const [res_UUID, setres_UUID] = React.useState("")
+    const router = useRouter();
+
 
     //Get res_UUID
     useEffect(() => {
@@ -57,22 +59,49 @@ export default function managerHomePage() {
     })
 
 
+    const deleteRestaurant = async () => {
+      try{
+        const response = await instance.post('/restaurant/delete', {
+          res_UUID
+        });
+        if (response.status == 200){
+          alert("Restaurant deleted.")
+          router.push('/pages/login-page');
+        } else{
+          alert("Restaurant deletion failed!");
+          andRefreshDisplay()        
+        }
+      } catch (err){
+        // Handle and log network errors
+        if (axios.isAxiosError(err)) {
+          console.error("Axios Error:", err.message);
+          console.error("Error Response:", err.response);
+          console.error("Error Request:", err.request);
+      } else {
+          console.error("Unexpected Error:", err);
+      }
+      }
+    }
+
+
+
     return(
-            <div>
-            <div className="active-restaurant">
-            <label className="active-sign">ACTIVE</label>
-            <div className="restaurant-info">
-                <label className="restaurant-name">Restaurant Name: {restaurantName}</label>
-                <label className="restaurant-address">Address: {address}</label>
-                <label className="restaurant-opentime">Opens at: {startTime}</label>
-                <label className="restaurant-closetime">Closes at: {closeTime}</label>
-            </div>
-            </div>
+      <div>
+        <div className="active-restaurant">
+              <label className="active-sign">ACTIVE</label>
+              <div className="restaurant-info">
+                  <label className="restaurant-name">Restaurant Name: {restaurantName}</label>
+                  <label className="restaurant-address">Address: {address}</label>
+                  <label className="restaurant-opentime">Opens at: {startTime}</label>
+                  <label className="restaurant-closetime">Closes at: {closeTime}</label>
+              </div>
+              {/* Delete Table */}
+              <div className="delete-restaurant">
+                <button className="delete-button" onClick={deleteRestaurant}>Delete Restaurant?</button>
+              </div>
 
 
-
-
-
-        </div >
+        </div>
+      </div >
     );
 }
