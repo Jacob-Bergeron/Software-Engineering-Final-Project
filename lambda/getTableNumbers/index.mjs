c533feeb96d7c0479b674f7de8502768bfaaf392
SkylerDooley <skylerdooley@gmail.com>
2024-12-06 13:48:37 -0500

    lots of small edits to manager homepage

---
+++ lambda/getTableNumbers/index.mjs
@@ -0,0 +1,47 @@
+import mysql from "mysql";
+
+export const handler = async (event) => {
+  var pool = mysql.createPool({
+    host: "cs3733db.c5ia86k2epli.us-east-2.rds.amazonaws.com",
+    user: "cs3733",
+    password: "database720$",
+    database: "Tables4u",
+  });
+
+  let getNumbers = (res_UUID) => {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        "SELECT tableNumber FROM TableInfo WHERE res_UUID = ?",
+        [res_UUID],
+        (error, rows) => {
+          if (error) {
+            return reject(error);
+          }
+          return resolve(rows);
+        }
+      );
+    });
+  };
+let response;
+
+  // this is what is returned to client
+  try{
+    const ans = await getNumbers(event.res_UUID);
+    response = {
+      statusCode: 200,
+      result: {
+        body: ans 
+      },
+    }
+  } catch(error){
+    response = {
+      statusCode : 400,
+      message : "Internal Error",
+      error : error.message
+    };
+  }
+   
+  pool.end(); // close DB connections
+
+  return response;
+};
