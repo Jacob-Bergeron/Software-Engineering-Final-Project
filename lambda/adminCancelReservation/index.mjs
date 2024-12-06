d0147d6e9b2f8b5b5d6f457a21fa8ab27c7566f9
Jacob Bergeron <jacobbergeron07@gmail.com>
2024-12-06 13:09:59 -0500

    lambda

---
+++ lambda/adminCancelReservation/index.mjs
@@ -0,0 +1,49 @@
+import mysql from 'mysql'
+
+export const handler = async (event) => {
+    var pool = mysql.createPool({
+        host: "cs3733db.c5ia86k2epli.us-east-2.rds.amazonaws.com",
+        user: "cs3733",
+        password: "database720$",
+        database: "Tables4u",
+        dateStrings: true
+    });
+
+    // Restaurant needs to be active
+    // Restaurant needs to have open tables
+    // Table need to be availble at given time 
+    // Table needs to be able to hold at least given amount of people
+
+    let getAvailableRestaurants = (date, time, numPeople) => {
+        return new Promise((resolve, reject) => {
+            pool.query("SELECT r.res_UUID, r.restaurantName, t.table_UUID, t.date FROM All_Restaurants r JOIN TableInfo t ON r.res_UUID = t.res_UUID WHERE r.isActive = 1 AND t.date = ? AND t.timeStart = ? AND t.isBooked = 0 AND t.numSeats >= ?",
+            [date,time,numPeople ],(error,rows) =>{
+                if(error) {return reject(error);}
+                else{return resolve(rows);}
+            });
+        });
+    };
+
+
+    let response;
+
+    try{
+        const ans = await getAvailableRestaurants(event.date, event.time, event.numPeople)
+        // this is what is returned to client
+        response = {
+            statusCode: 200,
+            result: {
+                body:ans
+            }
+        }
+    } catch (error){
+        response = {
+            statusCode : 400,
+            error : error.message
+          };
+    }
+    pool.end()      // close DB connections
+
+    return response;
+    
+}
